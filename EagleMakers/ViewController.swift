3cb51f56e3fabbe7ca0ffc0cd97652c281084db7
Gage Condon <condongb@bc.edu>
2021-12-04 14:03:13 -0500

    Initial Commit

---
+++ EagleMakers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EagleMakers
+//
+//  Created by Gage Condon on 12/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EagleMakersTests/EagleMakersTests.swift
@@ -0,0 +1,33 @@
+//
+//  EagleMakersTests.swift
+//  EagleMakersTests
+//
+//  Created by Gage Condon on 12/4/21.
+//
+
+import XCTest
+@testable import EagleMakers
+
+class EagleMakersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
